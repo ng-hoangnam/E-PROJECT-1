0d8e11ac652c86dea1c3dc3b906fd6f804e6c5b6
Nguyễn Tài Thành <taithanh95.humg@gmail.com>
2022-08-23 11:02:15 +0700

    Thêm các funcion cần dùng

--- php-control/product.php
+++ php-control/product.php
@@ -5,11 +5,10 @@
  * Date: 10/08/2022
  * Time: 2:45 PM
  */
-$connect = null;
 
 // Include File Config Database
 include('./php-control/config.php');
 
-$query = "SELECT * FROM Products";
-$res = $connect -> query($query);
-$rows = $res -> fetch_all(MYSQLI_ASSOC);
+$list = select_list("SELECT * FROM Product");
+var_dump($list);
+?>
