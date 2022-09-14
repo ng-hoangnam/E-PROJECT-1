3a246ca727ead06d1c614d242cf81b7de2ba4483
Nam Hoang Nguyen <nam.nh.2068@aptechlearning.edu.vn>
2022-09-14 22:28:14 +0700

    Delete-Comment

--- php-control/feedback.php
+++ php-control/feedback.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: Administrator <taithanh95.dev@gmail.com>
- * Date: 10/08/2022
- * Time: 2:45 PM
- */
 
 // Include File Config Database
 include('./php-control/config.php');
--- php-control/product.php
+++ php-control/product.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * Created by IntelliJ IDEA.
- * User: Administrator <taithanh95.dev@gmail.com>
- * Date: 10/08/2022
- * Time: 2:45 PM
- */
-
 // Include File Config Database
 include('./php-control/config.php');
 
@@ -20,3 +13,6 @@ if (isset($_GET['logout'])) {
 $sql = "SELECT * FROM products A JOIN brands B ON A.BRANDID = B.BRANDID";
 $product_list = select_list($sql);
 
+if(isset($_POST['addtocart'])) {
+    
+};
