0624387917e893a2b2ae8fb84dee24464137aeec
Nam Hoang Nguyen <107751086+ng-hoangnam@users.noreply.github.com>
2022-09-14 17:14:04 +0700

    Edit-Design

--- templates/header.php
+++ templates/header.php
@@ -6,6 +6,22 @@
             <i class="bi bi-phone d-flex align-items-center"><span>+84 85 247 0666</span></i>
             <i class="bi bi-clock d-flex align-items-center ms-4"><span> Mon-Sat: 9AM - 23PM</span></i>
         </div>
+
+        <?php
+            if(isset($_SESSION['username'])) { 
+            echo '
+            <form method="GET">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Hi, '. $_SESSION['customer_name'] .'
+                </a>
+                <ul class="dropdown-menu dropdown-menu-light">
+                <li><a class="dropdown-item" href="#">Profile</a></li>
+                <li><a class="dropdown-item" href="#">Change Password</a></li>
+                <li class="d-flex justify-content-end mt-4"><button class="btn btn-danger mx-2" type="submit" name="logout">Log out</button></li>
+                </ul>
+            </form>';
+            };
+        ?>
     </div>
 </div>
 
@@ -28,5 +44,29 @@
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
         </nav>
+
+        <!-- navbar login -->
+        <nav class="navbar login">
+            <div class="d-flex justify-content-end">
+            <?php 
+                if(!isset($_SESSION['username'])) {
+                    echo '
+                    <div class="dropdown"><a href="./login.html" class="nav-link" style="padding: 0;width: 100%;height: 100%;"><i
+                    class="bi bi bi-person" style="font-size: 30px;"></i></a>
+                    <ul>
+                        <li><a href="./login.html">Sign In</a></li>
+                        <li><a href="./register.html">Register</a></li>
+                    </ul>
+                    </div>
+                    ';
+                }
+                ?>
+
+            <div class="dropdown" style="margin-left: 28%;"><a href="./cart.html" class="nav-link"
+                style="padding: 0;width: 100%;height: 100%;"><i class="bi bi bi-cart" style="font-size: 30px;"></i></a>
+            </div>
+            </div>
+        </nav>
+        <!-- end navbar login -->
     </div>
 </header><!-- End Header -->
